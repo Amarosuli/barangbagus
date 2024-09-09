6f381b2e29315483a4dcf9b2aded279ed5d5ca6b
amarosuli <vuvlnvw@gmail.com>
2024-09-09 17:47:46 +0700

    add welcome section

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,4 +2,16 @@
 	import '../app.css';
 </script>
 
-<slot></slot>
+<div class="absolute top-0 -z-10 h-full w-full bg-stone-50">
+	<div
+		class="absolute bottom-auto left-auto right-0 top-0 h-[500px] w-[500px] -translate-x-[30%] translate-y-[20%] rounded-full bg-[rgba(173,109,244,0.5)] opacity-50 blur-[80px]"
+	></div>
+</div>
+<div class="container mx-auto">
+	<div class="flex h-12 w-full items-center justify-between border-b p-4 lg:p-8">
+		<h1 class="font-nerko-one text-xl text-purple-800 lg:text-3xl">😍 Barang Bagus</h1>
+		<h1 class="text-sm font-bold text-slate-600 lg:text-xl">Komentar & Testimonial</h1>
+	</div>
+
+	<slot></slot>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,39 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<section class="mt-8 flex w-full items-center justify-center p-12">
+	<div class="flex flex-col items-center justify-center lg:flex-row">
+		<img class="w-[80%] rounded-full pb-4 lg:hidden" src="./headline-welcome.jpg" alt="welcome" />
+		<div class="flex w-full flex-col lg:w-[40rem] lg:pr-12">
+			<h1
+				class="font-nerko-one text-6xl font-bold text-purple-700 lg:text-[6rem] lg:leading-[4.5rem]"
+			>
+				Selamat Datang di Barang Bagus
+			</h1>
+			<p class="hidden text-justify font-mono text-lg text-slate-700 lg:py-12">
+				Banyak hal yang bisa meningkatkan value dan kualitas dirimu. Barang Bagus adalah tempat
+				terbaik untuk produk digital yang kamu butuhkan. Kualitas adalah hal yang paling kami
+				utamakan, kami bahkan menyediakan kolom kritik dan saran untuk kebutuhan testimonial maupun
+				koreksi bagi kami. Jangan ragu, beli sekarang! Kualitas maksimal dengan harga yang masuk
+				akal.
+			</p>
+			<p class="py-6 text-justify text-lg text-slate-700">
+				Barang Bagus adalah tempat terbaik untuk produk digital yang kamu butuhkan. Jangan ragu,
+				beli sekarang! Kamu akan mendapatkan produk berkualitas dengan harga yang masuk akal.
+			</p>
+			<button
+				class="w-fit place-self-center rounded-full bg-purple-400 px-12 py-6 text-lg font-bold text-white shadow-md active:shadow-lg lg:place-self-start"
+				>Check All Products</button
+			>
+		</div>
+		<img
+			class="hidden w-[40%] rounded-full py-2 lg:block"
+			src="./headline-welcome.jpg"
+			alt="welcome"
+		/>
+	</div>
+</section>
+
+<section class="flex w-full flex-col items-center justify-center p-12">
+	<h1 class="font-nerko-one text-center text-4xl font-bold text-slate-700">
+		Pilih Produk Digital mu
+	</h1>
+	<p></p>
+</section>
